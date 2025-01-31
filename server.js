8bddff822e6a430a6b289e95f5554dd1e5ac3f0d
Adam Amusa <Adamamusa2004@gmail.com>
2025-01-31 18:37:36 +0000

    Image uploading working

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const app = express();
+import { uploadFile } from './FileService';
+import express from 'express';
+import multer from 'multer';
 
+const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 const port = 5000;
 
 app.get('/api', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(port, () => {console.log("Server is running on http://localhost:"+ port)});
+
+app.post('/api/upload', upload.single('file'), async (req, res) => {
+  try {
+    const result = await uploadFile(req.file);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('File upload failed');
+  }
+});
+
+app.listen(port, () => { console.log("Server is running on http://localhost:" + port) });
